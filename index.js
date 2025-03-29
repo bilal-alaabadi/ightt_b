df345c23502d5b316ff6287f71f2b391a064f778
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-03-30 03:50:22 +0400

    maindddf

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ app.use(
 );
 
 // دعم طلبات OPTIONS (Preflight Requests)
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://genuine-front-rho.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.send();
-})
+// app.options('*', (req, res) => {
+//     res.header('Access-Control-Allow-Origin', 'https://genuine-front-rho.vercel.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.send();
+// })
 
 // رفع الصور
 const uploadImage = require("./src/utils/uploadImage");
