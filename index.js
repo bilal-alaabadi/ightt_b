d4845c7ab94f7a871f724a2a5e286d8d12dfacd3
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-04-23 18:10:40 +0400

    Initial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // Middleware setup
 app.use(express.json({ limit: "25mb" }));
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        // origin:"https://genuine-f-delta.vercel.app",
-        origin: "http://genuineman.store",//مال الفرونت اند
+        origin:"https://genuine-f-delta.vercel.app",
+        // origin: "http://localhost:5173",//مال الفرونت اند
         credentials: true,
     })
 );
@@ -24,7 +24,7 @@ app.use(
 
 // دعم طلبات OPTIONS (Preflight Requests)
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://genuineman.store');
+    res.header('Access-Control-Allow-Origin', 'https://genuine-f-delta.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.send();
